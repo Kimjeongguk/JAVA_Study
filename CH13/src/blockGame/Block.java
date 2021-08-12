946ba4417c862238d330b89d765205ef196afd51
Kim Jeongguk <mindstar7949@naver.com>
2021-08-12 18:09:48 +0900

    ballGame

---
+++ CH13/src/blockGame/Block.java
@@ -0,0 +1,17 @@
+package blockGame;
+
+public class Block {
+	public int x;
+	public int y;
+	public int width;
+	public int height;
+	public int color;
+	public boolean isHide = false;
+	
+	public Block() {
+		this.width = GamePanel.BLOCK_WIDTH;
+		this.height = GamePanel.BLOCK_HEIGHT;
+		this.color = 0;
+		this.isHide = false;
+	}
+}
